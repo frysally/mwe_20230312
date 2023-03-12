00c7e5fa372f4e9f04de79ac34e116ada1d3cc09
frysally <61375378+frysally@users.noreply.github.com>
2023-03-12 01:02:13 -0500

    Create mwe.py

---
+++ mwe.py
@@ -0,0 +1,13 @@
+import numpy as np
+import dolfinx.mesh as mesh
+from dolfinx.io import XDMFFile
+from mpi4py import MPI
+
+infile = XDMFFile(MPI.COMM_WORLD, "mesh.xdmf", "r")
+domain = infile.read_mesh(name="notched_sample")
+infile.close()
+
+def bottom(x):
+    return np.isclose(x[1], -0.5)
+
+bottom_facets = mesh.locate_entities_boundary(domain, domain.geometry.dim-1, bottom)
